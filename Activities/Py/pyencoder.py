b0dff483ae06678b1dd07a43c4ddb6c5228b6c69
navintc <navin.thamindu@gmail.com>
2022-03-04 13:46:30 +0530

    Py encoder w/ imran

---
+++ Activities/Py/pyencoder.py
@@ -0,0 +1,32 @@
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+encoder = ['z','y','x','w','v','u','t','s','r','q','p','o','n','m','k','j','i','h','g','f','e','d','c','b','a']
+senten = "thunger fuck funker"
+
+listsen = list(senten)
+
+k=0
+
+for encodee in listsen:
+    flagJ=0
+    for letter in alphabet:
+        if encodee == letter :
+            listsen[k]=encoder[flagJ]  
+        flagJ+=1
+    k+=1
+    
+encodedShitSentence = "".join(listsen)
+print(encodedShitSentence)
+
+encodedShit = list(encodedShitSentence)
+
+k=0
+for encodee in encodedShit:
+    flagJ=0
+    for letter in encoder:
+        if encodee == letter :
+            encodedShit[k]=alphabet[flagJ]  
+        flagJ+=1
+    k+=1
+print("".join(encodedShit))
+
+
